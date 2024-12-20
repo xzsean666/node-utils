d6162a4978177f3b29cb76b19f6b5209f28a7a57
sean-astrid <xz_sean@qq.com>
2024-12-20 17:44:59 +0800

    1
    1

--- scripts/version-bump.js
+++ scripts/version-bump.js
@@ -1,26 +1,19 @@
-const fs = require('fs');
-const { execSync } = require('child_process');
+import fs from 'fs';
 
-try {
-  // 读取当前版本
-  const package = JSON.parse(fs.readFileSync('./package.json'));
-  const currentVersion = package.version;
-
-  // 更新补丁版本
-  const [major, minor, patch] = currentVersion.split('.');
-  const newVersion = `${major}.${minor}.${parseInt(patch) + 1}`;
-
+function bumpVersion() {
+  // 使用不同的变量名，避免使用 package 这个保留字
+  const pkgJson = JSON.parse(fs.readFileSync('./package.json'));
+  
+  // 分割版本号
+  const [major, minor, patch] = pkgJson.version.split('.').map(Number);
+  
+  // 增加补丁版本号
+  pkgJson.version = `${major}.${minor}.${patch + 1}`;
+  
   // 更新 package.json
-  package.version = newVersion;
-  fs.writeFileSync('./package.json', JSON.stringify(package, null, 2));
+  fs.writeFileSync('./package.json', JSON.stringify(pkgJson, null, 2));
+  
+  console.log(`Version bumped to ${pkgJson.version}`);
+}
 
-  // 提交更改
-  execSync('git add package.json');
-  execSync(`git commit -m "chore: bump version to ${newVersion}"`);
-  execSync(`git tag v${newVersion}`);
-
-  console.log(`Successfully bumped version to ${newVersion}`);
-} catch (error) {
-  console.error('Error updating version:', error);
-  process.exit(1);
-} 
+bumpVersion(); 

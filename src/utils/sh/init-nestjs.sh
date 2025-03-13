ab2f4b19dca66ba3f1693a5bfd053dbb52cd1683
sean-astrid <sean@astriddao.xyz>
2025-03-13 16:56:59 +0800

    1
    1

--- src/utils/sh/init-nestjs.sh
+++ src/utils/sh/init-nestjs.sh
@@ -13,13 +13,13 @@ cp src/utils/sh/nestjs.sh .
 # 给nestjs.sh添加可执行权限
 chmod +x nestjs.sh
 
-# 忽略src/helper/sdk和src/utils目录
-echo "src/helper/sdk/" >> .gitignore
+# 忽略src/helper/helpers/sdk和src/utils目录
+echo "src/helper/helpers/sdk/" >> .gitignore
 echo "src/utils/" >> .gitignore
 
 # 添加"build:sdk"命令到package.json
-npx json -I -f package.json -e 'this.scripts["build:sdk"] = "npx ts-node src/utils/scripts/copyDependencies.ts --input src/helper/sdk/index.ts --output src/main"'
+npx json -I -f package.json -e 'this.scripts["build:sdk"] = "npx ts-node src/utils/scripts/copyDependencies.ts --input src/helper/helpers/sdk/index.ts --output src/main"'
 
 
 # 修改tsconfig.build.json文件
-npx json -I -f tsconfig.build.json -e 'this.exclude.push("src/utils", "src/helper/sdk")'
+npx json -I -f tsconfig.build.json -e 'this.exclude.push("src/utils", "src/helper/helpers/sdk")'
